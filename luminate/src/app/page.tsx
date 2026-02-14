1516fa2dfa2cbd7cf1863ab17e781d3633834292
moul chi <moerrami@moncef.>
2026-02-15 00:28:53 +0100

    light/dark mode switch

--- luminate/src/app/page.tsx
+++ luminate/src/app/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import ThemeToggle from "@/components/shared/theme-toggle";
 
 export default function Home() {
-  return(
-  <div>
-    <h1>Hello world</h1>
-  </div>
-);
-
-  
+  return (
+    <div >
+      <div className="w-full flex justify-end">
+        <ThemeToggle/>
+      </div>
+    </div>
+    
+  );
 }
