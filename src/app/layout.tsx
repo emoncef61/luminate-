8b3a9308f28fb12030d8d61a3c51785fa5c3a1cd
moul chi <moerrami@moncef.>
2026-02-16 18:10:04 +0100

    fix inner folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,7 +53,7 @@ export default function RootLayout({
       >
         <ThemeProvider 
           attribute="class"
-          defaultTheme="dark"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
